41a4a8a834a15f200350145b0b88f3af37a053fd
Kristina Egorova <kristi.egorova@gmail.com>
2023-11-18 16:50:22 +0300

    fixed template error

--- src/test/java/specs/CreateUserSpec.java
+++ src/test/java/specs/CreateUserSpec.java
@@ -1,5 +1,6 @@
 package specs;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
